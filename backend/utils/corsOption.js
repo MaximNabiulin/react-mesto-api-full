ef85da90319ab9dc92719cc0f0b20eb50c1fbbae
MaximNabiulin <nabiulin.m.kh@gmail.com>
2022-11-16 22:02:10 +0500

    fix: дополнены опции для cors

--- backend/utils/corsOption.js
+++ backend/utils/corsOption.js
@@ -1,7 +1,10 @@
 module.exports.corsOptions = {
   origin: [
-    'https://mesto.nabiulin.nomoredomains.icu',
+    'localhost:3000',
     'http://localhost:3000',
+    'https://localhost:3000',
+    'http://mesto.nabiulin.nomoredomains.icu',
+    'https://mesto.nabiulin.nomoredomains.icu',
   ],
   // optionsSuccessStatus: 200,
   allowedHeaders: ['Content-Type', 'Authorization'],
